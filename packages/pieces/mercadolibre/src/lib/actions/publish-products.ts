bed799dc1cc6fd424aa60ff1b659a1994acf7f29
Mariano Diaz <mdiaz@lagom.us>
2024-01-19 14:52:40 -0300

    LEC-2935: Support product update in Synchronization to ML

--- packages/pieces/mercadolibre/src/lib/actions/publish-products.ts
+++ packages/pieces/mercadolibre/src/lib/actions/publish-products.ts
@@ -1,4 +1,8 @@
-import { createAction, Property } from '@activepieces/pieces-framework';
+import {
+  createAction,
+  Property,
+  StoreScope,
+} from '@activepieces/pieces-framework';
 import { httpClient, HttpMethod } from '@activepieces/pieces-common';
 import { meliAuth } from '../..';
 import { Attribute, Item, Product } from '../common/models';
@@ -25,33 +29,90 @@ export const publish_products = createAction({
     const cdnBaseUrl = context.propsValue['cdnBaseUrl'];
     const token = context.auth.access_token;
 
-    const categoryId = getCategoryId(item.attributeSetName);
+    // check wether the product is already published
+    const sku = item.attributes['sku'];
+    const mlaId = await context.store.get<string>(sku, StoreScope.FLOW);
 
-    if (categoryId) {
-      const persistedAttrs = await context.store.get<string>(categoryId);
-      let attributes = persistedAttrs ? JSON.parse(persistedAttrs) : undefined;
-      if (!attributes) {
-        attributes = (
-          await httpClient.sendRequest<Attribute[]>({
-            method: HttpMethod.GET,
-            headers: { Authorization: `Bearer ${token}` },
-            url: `https://api.mercadolibre.com/categories/${categoryId}/attributes`,
-          })
-        ).body;
-        await context.store.put<string>(categoryId, JSON.stringify(attributes));
-      }
-
-      const body = generateProduct(item, attributes, cdnBaseUrl);
-
+    if (mlaId !== undefined && mlaId !== null) {
+      // EDIT EXISTING PRODUCT
       try {
         await httpClient.sendRequest<Item>({
-          method: HttpMethod.POST,
+          method: HttpMethod.PUT,
           headers: { Authorization: `Bearer ${token}` },
-          url: 'https://api.mercadolibre.com/items',
-          body: body,
+          url: `https://api.mercadolibre.com/items/${mlaId}`,
+          body: {
+            ...{ available_quantity: item.qty },
+            ...{ price: item.price },
+            ...{
+              pictures: item.attributes['images']?.map((i) => ({
+                source: `${cdnBaseUrl}/${i.id}`,
+              })),
+            },
+          },
+        });
+
+        await httpClient.sendRequest<Item>({
+          method: HttpMethod.PUT,
+          headers: { Authorization: `Bearer ${token}` },
+          url: `https://api.mercadolibre.com/items/${mlaId}/description?api_version=2`,
+          body: {
+            plain_text: item.attributes['shortDescription'],
+          },
         });
       } catch (e) {
-        context.store.put<string>(item.id, JSON.stringify(e));
+        context.store.put<string>(
+          item.id,
+          JSON.stringify(e),
+          StoreScope.PROJECT
+        );
+      }
+    } else {
+      // PUBLISH NEW PRODUCT
+      const categoryId = getCategoryId(item.attributeSetName);
+
+      if (categoryId) {
+        const persistedAttrs = await context.store.get<string>(
+          categoryId,
+          StoreScope.FLOW
+        );
+        let attributes = persistedAttrs
+          ? JSON.parse(persistedAttrs)
+          : undefined;
+        if (!attributes) {
+          attributes = (
+            await httpClient.sendRequest<Attribute[]>({
+              method: HttpMethod.GET,
+              headers: { Authorization: `Bearer ${token}` },
+              url: `https://api.mercadolibre.com/categories/${categoryId}/attributes`,
+            })
+          ).body;
+          await context.store.put<string>(
+            categoryId,
+            JSON.stringify(attributes),
+            StoreScope.FLOW
+          );
+        }
+
+        const body = generateProduct(item, attributes, cdnBaseUrl);
+
+        try {
+          const published = await httpClient.sendRequest<Item>({
+            method: HttpMethod.POST,
+            headers: { Authorization: `Bearer ${token}` },
+            url: 'https://api.mercadolibre.com/items',
+            body: body,
+          });
+          await context.store.put<string>(
+            item.attributes['sku'],
+            published.body.id
+          );
+        } catch (e) {
+          context.store.put<string>(
+            item.id,
+            JSON.stringify(e),
+            StoreScope.PROJECT
+          );
+        }
       }
     }
 
@@ -89,7 +150,7 @@ function generateProduct(
     price: item.price,
     currency_id: 'ARS',
     available_quantity: item.qty,
-    description: item.attributes['shortDescription'],
+    description: { plain_text: item.attributes['shortDescription'] },
     pictures: item.attributes['images']?.map((i) => ({
       source: `${cdnBaseUrl}/${i.id}`,
     })),
